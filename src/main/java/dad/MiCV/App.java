1aecea0e91995bde5ad738703943ff40278b2643
Alejandro123314 <alejandrojimenezclaro@gmail.com>
2022-11-21 11:13:58 +0000

    views hechos

--- src/main/java/dad/MiCV/App.java
+++ src/main/java/dad/MiCV/App.java
@@ -7,31 +7,28 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import dad.MiCV.controller.MainController;
 import dad.MiCV.model.Nacionalidad;
+import dad.ahorcado.AhorcadoApp;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceDialog;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class App extends Application {
 
+	public static Window primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		/*URI uri = App.class.getResource("csvnacionalidades.csv/").toURI();
-		List<String> items =Files.readAllLines(Paths.get(uri), StandardCharsets.UTF_8);
-		List<Nacionalidad> nacionaliades = items.stream()
-					.map(Nacionalidad::new)
-					.collect(Collectors.toList());
+		App.primaryStage = primaryStage;
 		
-		ChoiceDialog<Nacionalidad> dialog = new ChoiceDialog<Nacionalidad>();
-		dialog.getItems().setAll(nacionaliades);
-		dialog.setSelectedItem(nacionaliades.get(0));
-		dialog.setTitle("Eleccion");
-		dialog.setHeaderText("Elige una opción");
-		dialog.setContentText("Opción:");
-		Nacionalidad n = dialog.showAndWait().orElse(null);
-		System.out.println(n);*/
+		primaryStage.setTitle("Ahorcado");
+		primaryStage.setScene(new Scene(MainController.getView()));
+		primaryStage.show();
+		
 	}
-
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/main/java/dad/MiCV/controller/MainController.java
+++ src/main/java/dad/MiCV/controller/MainController.java
@@ -1,5 +1,69 @@
 package dad.MiCV.controller;
 
-public class MainController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+
+public class MainController implements Initializable {
+	
+	
+	
+	
+	 @FXML
+	    private Menu AyudaMenu;
+
+	    @FXML
+	    private Menu archivoMenu;
+
+	    @FXML
+	    private Tab conocimientosTab;
+
+	    @FXML
+	    private Tab contactoTab;
+
+	    @FXML
+	    private Tab experienciaTab;
+
+	    @FXML
+	    private Tab formacionTab;
+
+	    @FXML
+	    private MenuBar menuBar;
+
+	    @FXML
+	    private Tab personalTab;
+
+	    @FXML
+	    private BorderPane root;
+
+	    @FXML
+	    private TabPane tabPane;
+	    
+	    public MainController() throws IOException {
+	    	
+	    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+			loader.setController(this);
+			loader.load();
+		}
+
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		public TabPane getView() {
+			return tabPane;
+
+		}
 
 }
